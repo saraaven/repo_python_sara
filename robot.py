d26fcaf7ddc431e2bd875294c2e5855c2f0cdbf1
saraaven <aven.sara@gmail.com>
2015-08-05 14:36:04 +0200

    #7 Skriv om programmet med lista av produkter

--- robot.py
+++ robot.py
@@ -17,11 +17,11 @@ class Product(object):
 	def price_with_vat(self):
 		return self.price * self.count * self.vat
 			
+products = [Product(price = 900, count = 2, vat = 1.25), \
+Product(price = 100, count = 1, vat = 1.06)] #lista
 
-robot = Product(price = 900, count = 2, vat = 1.25)
-book = Product(price = 100, count = 1, vat = 1.06)
+total_price = products[0].price_with_vat() + products[1].price_with_vat() #ny funktion, där jag sammanställer priset för produkterna. 
 
-
-print robot.price_with_vat() + book.price_with_vat()
+print total_price
 
 
