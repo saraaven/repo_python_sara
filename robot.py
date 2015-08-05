a03c911bb5c179f62c8f6120af73b8c0e5de082a
saraaven <aven.sara@gmail.com>
2015-08-05 11:05:50 +0200

    #1 Startprogram
    Inserted variables

--- robot.py
+++ robot.py
@@ -7,5 +7,13 @@
 # En kund vill köpa: Två robotar (900 kr/st),  en instruktionsbok (100 kr/st)
 # När du räknar ut det, kom ihåg att böcker har 6% moms, inte 25%
 
-robot_price = 900 * 2 * 1.25 + 100 * 1 * 1.06
-print robot_price
+#robot_price = 900 * 2 * 1.25 + 100 * 1 * 1.06
+robot_price = 900
+robot_count = 2
+robot_vat = 1.25
+
+book_price = 100 
+book_count = 1
+book_vat = 1.06
+
+print (robot_price * robot_count * robot_vat + book_price * book_count * book_vat)
