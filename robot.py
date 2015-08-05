53795d7dad89fb6869c3def9b97755be5893006e
saraaven <aven.sara@gmail.com>
2015-08-05 11:56:11 +0200

    #4 Class

--- robot.py
+++ robot.py
@@ -7,8 +7,21 @@
 # En kund vill köpa: Två robotar (900 kr/st),  en instruktionsbok (100 kr/st)
 # När du räknar ut det, kom ihåg att böcker har 6% moms, inte 25%
 
-robot = {"price": 900, "count": 2, "vat": 1.25}
-book = {"price": 100, "count": 1, "vat": 1.06}
+class Product(object):
+	price = 0
+	count = 0
+	vat = 0
 
-print robot["price"] * robot["count"] * robot["vat"] + book["price"] * book["count"] * book["vat"]
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.vat = 1.25
+
+book = Product()
+book.price = 100
+book.count = 1
+book.vat = 1.06
+
+print robot.price * robot.count * robot.vat + book.price * book.count * book.vat
+
 
